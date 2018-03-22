cfecb858e9c176f6c35db7cf20b458c4556d546b
Camilla Tranberg <camillatranberg@Camillas-MacBook-Pro.local>
2018-03-22 10:54:11 +0100

    Adding css modules.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './App.css';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className={styles.div}>
       </div>
     );
   }
