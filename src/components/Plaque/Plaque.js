fbb9ff4672e1c4e8dc9750d0d9d9733464ac81cc
ninasjoberg <nina.sjoberg@gmail.com>
2018-04-18 22:22:53 +0200

    portrait dimensions fixed

--- src/components/Plaque/Plaque.js
+++ src/components/Plaque/Plaque.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Plaque.module.css';
 
-const Plaque = ({ artist, artName, handlePlaqueClick, index }) => (
+const Plaque = ({ artist, handlePlaqueClick, index }) => (
   <div className={styles.plaque} onClick={(e) => handlePlaqueClick(e, index)}>
     <h2 className={styles.plaqueHeading}>POM GALLERY</h2>
     <p className={styles.plaqueText}>{artist}</p>
